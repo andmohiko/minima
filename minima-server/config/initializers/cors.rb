68c610a45a2ad0ad04385ae3b1ca814544b6606e
andmohiko <thomasito.berryz@gmail.com>
2020-08-25 10:16:50 +0900

    deploy config

--- minima-server/config/initializers/cors.rb
+++ minima-server/config/initializers/cors.rb
@@ -7,7 +7,12 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    # origins '*'
+    if Rails.env.production?
+        origins 'https://minima-5f754.web.app/'
+    else
+        origins 'http://localhost:3000'
+    end
 
     resource '*',
       headers: :any,
